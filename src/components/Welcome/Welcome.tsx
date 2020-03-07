171209577f290f4b9a68c6f762a6951a60863d23
jundymek <jundymek@gmail.com>
2020-03-07 21:41:42 +0100

    chore: meta data, favicon.ico

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -10,7 +10,7 @@ function Welcome({ isVisible = true, setIsWelcomeVisible }: Props) {
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>Welcome to charity finder</h2>
-      <p className={styles.paragraph}>This simple app allows you to search a list of global charities. Simply find a charity that matches your interests. <span role="img" aria-label="Heart icon">❤️</span></p>
+      <p className={styles.paragraph}>This simple app allows you to search for a list of global charities. Simply find a charity that matches your interests. <span role="img" aria-label="Heart icon">❤️</span></p>
       <button className={styles.button} onClick={() => setIsWelcomeVisible(false)}>Proceed to site</button>
     </section>
   );
