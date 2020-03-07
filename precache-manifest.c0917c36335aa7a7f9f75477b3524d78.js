c48497bb8e7ffaddc806f00ea74ed577a740b40e
jundymek <jundymek@gmail.com>
2020-03-07 21:53:07 +0100

    Updates

---
+++ precache-manifest.c0917c36335aa7a7f9f75477b3524d78.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35e225df4ba69e831aeb6941cd93c83",
+    "url": "/charity-finder/index.html"
+  },
+  {
+    "revision": "f9338ca114ff632bf892",
+    "url": "/charity-finder/static/css/2.5a086f31.chunk.css"
+  },
+  {
+    "revision": "55f82672c69ec11ea3d2",
+    "url": "/charity-finder/static/css/main.7ae86808.chunk.css"
+  },
+  {
+    "revision": "f9338ca114ff632bf892",
+    "url": "/charity-finder/static/js/2.26b9b65b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/charity-finder/static/js/2.26b9b65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55f82672c69ec11ea3d2",
+    "url": "/charity-finder/static/js/main.439006a8.chunk.js"
+  },
+  {
+    "revision": "ea99c24a19d1f33bc08b",
+    "url": "/charity-finder/static/js/runtime-main.6d4b4e92.js"
+  },
+  {
+    "revision": "e0edbb12df2e0d6ed2a2253546a5b4b4",
+    "url": "/charity-finder/static/media/github-icon.e0edbb12.svg"
+  },
+  {
+    "revision": "54f06be9791a3f7175d38edfc04d5393",
+    "url": "/charity-finder/static/media/linkedin-icon.54f06be9.svg"
+  },
+  {
+    "revision": "c98f5eecadb4f73cb674ecc8bbf3f1c4",
+    "url": "/charity-finder/static/media/location-icon.c98f5eec.svg"
+  },
+  {
+    "revision": "0ee7f37adbac6c795672e61418c1962c",
+    "url": "/charity-finder/static/media/welcome_bg.0ee7f37a.jpg"
+  }
+]);
