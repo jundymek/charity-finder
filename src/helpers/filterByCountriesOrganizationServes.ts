30f6591c43cc1b69d57eeb84afa5e9cd9994e58f
jundymek <jundymek@gmail.com>
2020-03-10 23:36:51 +0100

    fix: check countries in filterByCountriesOrganizationServes

--- src/helpers/filterByCountriesOrganizationServes.ts
+++ src/helpers/filterByCountriesOrganizationServes.ts
@@ -1,7 +1,7 @@
 import { Charity, SelectedCountry } from "./types";
 
-export const filterByCountriesOrganizationServes = (data: Charity[], countries: SelectedCountry[]) => {
-  if (countries.length) {
+export const filterByCountriesOrganizationServes = (data: Charity[], countries: SelectedCountry[]): Charity[] => {
+  if (countries && countries.length) {
     return data.filter((project: Charity) =>
       project.organizationCountries.some((item: string) =>
         countries.map(item => item.label).includes(item)
